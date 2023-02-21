6a592d5bcc5e28c0641d7f4871df761dbbf78d5d
ryansummerlin <ryansummerlin777@gmail.com>
2023-02-20 18:43:38 -0800

    Completed the breadth first search, passed all tests

--- bfs-path.js
+++ bfs-path.js
@@ -1,24 +1,55 @@
 function findNeighbors(node, matrix) {
+    let neighbors = [];
+    let row = node[0];
+    let col = node[1];
     // Up
-
+    if (row > 0) {
+        neighbors.push([row - 1, col]);
+    }
     // Down
-
+    if (row < matrix.length - 1) {
+        neighbors.push([row + 1, col]);
+    }
     // Left
-
+    if (col > 0) {
+        neighbors.push([row, col - 1]);
+    }
     // Right
+    if (col < matrix[0].length - 1) {
+        neighbors.push([row, col + 1])
+    }
 
-    // Your code here
+    return neighbors;
 }
 
 
 function bfsPath(matrix, startNode, endValue) {
-    // Your code here
+    let queue = [startNode];
+    let visited = new Set();
+    let searched = [];
+
+    while (queue.length !== 0) {
+        let currentNode = queue.shift();
+        if (!visited.has(currentNode.join(','))) {
+            visited.add(currentNode.join(','));
+            searched.push(currentNode);
+
+            if (matrix[currentNode[0]][currentNode[1]] === endValue) {
+                return searched;
+            }
+
+            let neighbors = findNeighbors(currentNode, matrix);
+            queue = [...queue, ...neighbors];
+        }
+    }
+
+    return false;
 }
 
 
 // ***** UNCOMMENT FOR LOCAL TESTING *****
 
-// const matrix1 = [ 
+// const matrix1 = [
 //     [  1,  2,  3,  4 ],
 //     [  5,  6,  7,  8 ],
 //     [  9, 10, 11, 12 ],
@@ -60,11 +91,11 @@ function bfsPath(matrix, startNode, endValue) {
 // // value is located at start node
 // // [ [ 2, 2 ] ]
 
-// console.log(bfsPath(matrix1, [1,2], 8)); // can handle various start nodes 
+// console.log(bfsPath(matrix1, [1,2], 8)); // can handle various start nodes
 // // and end values
 // // [ [ 1, 2 ], [ 0, 2 ], [ 2, 2 ], [ 1, 1 ], [ 1, 3 ] ]
 
-// console.log(bfsPath(matrix1, [0,0], 17)); // can return false if end value 
+// console.log(bfsPath(matrix1, [0,0], 17)); // can return false if end value
 // // is not found
 // // false
 
